ae7dc7bbba786e9022724b68c4d7def44c4b9512
benjaminmehlberg <benjamin.mehlberg@gmail.com>
2021-09-25 14:34:08 +0200

    Debugging

--- textadventure.py
+++ textadventure.py
@@ -9,8 +9,10 @@ from modules import Room, Character, Enemy, Friend, Item, RPGInfo
 def show_inventory(inventory):
 
     print("\n" + "<-- Your inventory -->".center(30))
-    print("<Empty>".center(30)) if inventory == [] else
-           [print(item.name.center(30)) for item in inventory]
+    if inventory == []:
+        print("<Empty>".center(30))
+    else:
+        [print(item.name.center(30)) for item in inventory]
     print("<-------------------->".center(30))
     print("\n")
 
